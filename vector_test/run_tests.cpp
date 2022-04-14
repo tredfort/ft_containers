b0360e48deb0c175e3007a9141c6a195ffa25f65
rxxxt <d.rinat703@gmail.com>
2022-04-15 00:11:40 +0300

    delete unnecessary

--- vector_test/run_tests.cpp
+++
@@ -1,9 +0,0 @@
-#include "vector_test.hpp"
-
-int main() {
-    std::srand(time(0));
-    cout << endl <<  BYELLOW << "    *** running tests for a vector ***" << "   |     time" << RESET << endl;
-    cout << (constructor_vector() ? (BGREEN + "OK") : (BRED + "KO")) << RESET "    " << std::to_string(g_std_time) << " | " << std::to_string(g_ft_time);
-    cout << endl;
-    return 0;
-}
